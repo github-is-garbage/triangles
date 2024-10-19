a06d5e1dbe02d2c192ec3ffaf76494a0d3cf08b8
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-10-18 23:05:33 -0400

    Test rendering a triangle

--- static/render.js
+++ static/render.js
@@ -1,4 +1,21 @@
-export function RenderTriangle(P1, P2, P3)
+export function DrawLine(Context, x1, y1, x2, y2)
 {
+	Context.beginPath()
+	{
+		Context.moveTo(x1, y1)
+		Context.lineTo(x2, y2)
+	}
+	Context.stroke()
+}
 
+export function RenderTriangle(x1, y1, x2, y2, x3, y3)
+{
+	const Canvas = document.querySelector("canvas")
+	const Context = Canvas.getContext("2d")
+
+	Context.strokeStyle = "black"
+
+	DrawLine(Context, x1, y1, x2, y2)
+	DrawLine(Context, x2, y2, x3, y3)
+	DrawLine(Context, x3, y3, x1, y1)
 }
--- static/triangles.js
+++ static/triangles.js
@@ -4,17 +4,8 @@ window.addEventListener("load", () =>
 {
 	const Canvas = document.querySelector("canvas")
 
-	if (!Canvas)
-	{
-		console.error("Canvas not found!")
-		return
-	}
-
 	Canvas.width = 1000
 	Canvas.height = 1000
 
-	const Context = Canvas.getContext("2d")
-
-	Context.fillStyle = "red"
-	Context.fillRect(0, 0, 1000, 1000)
+	RenderTriangle(20, 20, 200, 100, 50, 500)
 })
