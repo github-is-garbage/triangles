f6ea98fad830115288596187869558a69cc614d7
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-10-19 17:04:48 -0400

    Use better rendering technique
    Get the delta frame time for smoothness

--- static/scripts/triangles.js
+++ static/scripts/triangles.js
@@ -1,25 +1,51 @@
 import { Triangle } from "./triangle.js"
 
-window.addEventListener("load", () =>
+var IdealFrameTime = 1000 / 60 // 60 fps
+var LastFrameTime = null
+
+const Triangles = []
+
+function RenderTriangles(RenderContext, DeltaFrameTime)
+{
+	for (const Tri of Triangles)
+	{
+		Tri.RotatePoints(1 * DeltaFrameTime)
+
+		Tri.RenderToContext(RenderContext)
+		Tri.RenderPointsToContext(RenderContext)
+	}
+}
+
+function AnimateFrame()
 {
 	const Canvas = document.querySelector("canvas")
-	const Context = Canvas.getContext("2d")
+	if (!Canvas) return
 
+	const RenderContext = Canvas.getContext("2d")
+	RenderContext.clearRect(0, 0, Canvas.width, Canvas.height)
+
+	// Try to get 60 fps
+	if (!LastFrameTime)
+		LastFrameTime = Date.now()
+
+	const CurrentFrameTime = Date.now()
+	const DeltaFrameTime = (CurrentFrameTime - LastFrameTime) / IdealFrameTime
+
+	LastFrameTime = CurrentFrameTime
+
+	RenderTriangles(RenderContext, DeltaFrameTime)
+
+	// Garrr
+	requestAnimationFrame(AnimateFrame)
+}
+
+window.addEventListener("load", () =>
+{
 	const TestTri = new Triangle(150, 100, 200, 175, 100, 175)
 	TestTri.SetBorderColor(0, 255, 0, 0.5)
 	TestTri.SetFillColor(0, 0, 255, 0.25)
 
-	function RotateTestTri()
-	{
-		Context.clearRect(0, 0, Canvas.width, Canvas.height)
+	Triangles.push(TestTri)
 
-		TestTri.RotatePoints(1)
-
-		TestTri.RenderToContext(Context)
-		TestTri.RenderPointsToContext(Context)
-
-		requestAnimationFrame(RotateTestTri)
-	}
-
-	RotateTestTri()
+	AnimateFrame()
 })
