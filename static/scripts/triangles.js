142806517ff6a0aa276eb9a24abcc82f3564d9a8
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-10-18 23:16:50 -0400

    Make canvas 512x512 pixels

--- static/scripts/triangles.js
+++ static/scripts/triangles.js
@@ -4,9 +4,6 @@ window.addEventListener("load", () =>
 {
 	const Canvas = document.querySelector("canvas")
 
-	Canvas.width = 1000
-	Canvas.height = 1000
-
 	RenderTriangle(20, 20, 200, 100, 50, 500, "rgba(0, 255, 0, 0.5)", "rgba(0, 0, 255, 0.25)")
 	RenderTrianglePoints(20, 20, 200, 100, 50, 500)
 })
